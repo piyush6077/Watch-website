0b6b6eaf387b83704dde72e6ea5f4070deeccbf8
Piyush patil <098piyushpatil@gmail.com>
2024-01-31 23:51:09 +0530

    minor responsive bugs fixed

--- brain.js
+++ brain.js
@@ -3,20 +3,14 @@ const closeMenu = document.getElementById("closeMenu");
 const menu = document.getElementById("navSub");
 
 openMenu.addEventListener("click",()=>{
-    closeMenu.classList.remove("hidden");
-    menu.classList.remove("hidden");
-    openMenu.classList.add("hidden");
+    menu.classList.remove('hidden');
 });
 
 const Close_Menu = () => {
-       // closeMenu.classList.add("show");
-       openMenu.classList.add("show");
-       openMenu.classList.remove("hidden");
        menu.classList.add("hidden");
 };
 
 closeMenu.addEventListener("click", Close_Menu);
-
 
 
    
